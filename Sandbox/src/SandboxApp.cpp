5a32d06235bc870a40c9bb2a32a79ab8b3c1229b
DeclanGuy <declanguy2006@gmail.com>
2026-06-25 13:06:33 +0100

    Added glm submodule

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -3,7 +3,10 @@
 class Sandbox : public Hazel::Application {
 public:
 	Sandbox() {
-
+		HZ_INFO("Sandbox Running");
+#ifdef _DEBUG
+		HZ_CORE_INFO("Running in debug; optimisations disabled");
+#endif
 	}
 	~Sandbox() {
 
